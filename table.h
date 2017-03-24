44da978dce7da37aa04da798050597ec667683dd
allsey87 <allsey87@gmail.com>
2017-03-24 06:28:53 -0400

    Implemented element-wise access (sort of)

--- table.h
+++ table.h
@@ -7,16 +7,46 @@ template <class T, class Container = std::list<T> >
 class table {
 public:
    // Constructors
-   table(unsigned int rows, unsigned int cols) {
+   table(unsigned int rows, unsigned int cols) :
+      m_rows(rows),
+      m_cols(cols) {
       c.resize(rows * cols);
    }
    
-   // returntype rows() {}
-
+   // Element-wise access
+   typename Container::iterator begin() {
+      return std::begin(c);      
+   }
+   
+   typename Container::iterator end() {
+      return std::end(c);
+   }
+   
+   /*
+   auto rows() {
+      return struct {
+         row_view_iterator begin = the_begin; 
+         row_view_iterator end = the_end;
+      };
+   }
+   
+   void some_other_function(row_iterator it_row) {
+      row = *it_row;
+      
+   }
+   
+   class row_view {
+      row_view(table<T>& table, unsigned int index) :
+         m_table(table),
+         m_index(index) {}
+      table<T>& m_table;
+      unsigned int m_index;
+   };
+   */
 private:
+   unsigned int m_rows;
+   unsigned int m_cols;
+   
    Container c;
 };
-
 #endif
-
-
--- testbench.cpp
+++ testbench.cpp
@@ -1,24 +1,32 @@
 #include "table.h"
 
 #include <vector>
+#include <iostream>
 
 int main(int argc, char* args[]) {
    // test constructors, template parameters
    table<double> my_table(4,3);
    table<double, std::vector<double> > my_vec_table(3,4);
 
+     
+   for(double x : my_table) {
+      std::cout << x;
+   }
+      
+   //auto itRowBegin = my_table.row_begin();
+   
+   //table<double>::row_view my_row;
+   
    // row and column iterators
-   /*
    // for ranged-for to work my_table.rows() must return a struct or class with members begin() and end()
-   for(const table<double>::row& tr : my_table.rows()) {
-      for(auto el : tr) {
-         std::cout << el << ", ";      
-      }
-      std::cout << std::endl;
+   
+   /*
+   for(table<double>::row_view tr : my_table.rows()) {
+      tr[2] = 3;
    }
    
    for(const table<double>::column& tc : my_table.columns()) {
-      for(auto el : tc) {
+      for(double : tc) {
          std::cout << el << ", ";      
       }
       std::cout << std::endl;
