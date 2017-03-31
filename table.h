5961feab6f1e9f840c721451c02b0727911d8b13
allsey87 <allsey87@gmail.com>
2017-03-31 07:46:25 -0400

    Added row class with a naughty iterator

--- table.h
+++ table.h
@@ -1,16 +1,30 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+
 #include <list>
 
 template <class T, class Container = std::list<T> >
 class table {
+public:
+   typedef typename Container::value_type       value_type;
+   typedef typename Container::reference        reference;
+   typedef typename Container::const_reference  const_reference;
+   typedef typename Container::size_type        size_type;
+   typedef          Container                   container_type;
+   
 public:
    // Constructors
    table(unsigned int rows, unsigned int cols) :
       m_rows(rows),
       m_cols(cols) {
       c.resize(rows * cols);
+   }
+   
+   table(unsigned int rows, unsigned int cols, std::initializer_list<value_type> values) :
+      m_rows(rows),
+      m_cols(cols) {
+      c.assign(values);
    }
    
    // Element-wise access
@@ -23,26 +37,80 @@ public:
    }
    
    /*
-   auto rows() {
-      return struct {
-         row_view_iterator begin = the_begin; 
-         row_view_iterator end = the_end;
+   struct row_view_range {
+      row_view_iterator m_begin;
+      row_view_iterator m_end;     
+      auto begin() {
+         return m_begin;
       };
-   }
+      auto end() {
+         return m_end;  
+      };
+   };
+   */
    
-   void some_other_function(row_iterator it_row) {
-      row = *it_row;
+   class row {
+   public:
+      class iterator {
+      public:
+         iterator(row& _row, unsigned int _index = 0) :
+            m_row(_row),
+            m_index(_index) {}
+         
+         iterator(const iterator& iter) :
+            m_row(iter.m_row),
+            m_index(iter.m_index) {}
+         
+         ~iterator() {}
+
+         iterator& operator=(const iterator& iter) {
+            m_index = iter.m_index;
+         }
+        
+         bool operator==(const iterator& iter) const {
+            return (m_index == iter.m_index);
+         }
+        
+         bool operator!=(const iterator& iter) const {
+            return (m_index != iter.m_index);
+         }
+         
+         iterator& operator++() {
+            ++m_index;
+            return *this;
+         }
+         
+         value_type& operator*() {
+            typename Container::iterator it = std::begin(m_row.m_table.c);
+            std::advance(it, m_row.m_table.m_cols * m_row.m_index + m_index);
+            return *it;
+         }
+        
+      private:
+         row& m_row;
+         unsigned int m_index;
+      };
       
-   }
-   
-   class row_view {
-      row_view(table<T>& table, unsigned int index) :
+      iterator begin() {
+         return iterator(*this);
+      }
+      
+      iterator end() {
+         return iterator(*this,m_table.m_cols);
+      }
+      
+      row(table<T>& table, unsigned int index) :
          m_table(table),
          m_index(index) {}
+      
+      /* contents! */
+      //bool operator==(const row&) const {}  
+      
+   private:
       table<T>& m_table;
       unsigned int m_index;
    };
-   */
+  
 private:
    unsigned int m_rows;
    unsigned int m_cols;
--- testbench.cpp
+++ testbench.cpp
@@ -4,36 +4,22 @@
 #include <iostream>
 
 int main(int argc, char* args[]) {
-   // test constructors, template parameters
-   table<double> my_table(4,3);
-   table<double, std::vector<double> > my_vec_table(3,4);
-
-     
-   for(double x : my_table) {
-      std::cout << x;
-   }
+   //table<double, std::vector<double> > my_table(3,4);
+   table<double> my_table(4,3, {
+      1,2,3,
+      4,5,6,
+      7,8,9,
+      10,11,12,
+   });
       
-   //auto itRowBegin = my_table.row_begin();
+   table<double>::row myRow(my_table, 3);
    
-   //table<double>::row_view my_row;
-   
-   // row and column iterators
-   // for ranged-for to work my_table.rows() must return a struct or class with members begin() and end()
-   
-   /*
-   for(table<double>::row_view tr : my_table.rows()) {
-      tr[2] = 3;
+   for(double f : myRow) {
+      std::cout << f << ", ";
    }
-   
-   for(const table<double>::column& tc : my_table.columns()) {
-      for(double : tc) {
-         std::cout << el << ", ";      
-      }
-      std::cout << std::endl;
-   }
-   */
    
    return 0;
 }
 
 
+  
